e85806d362de70f1581bffa9296cefa5856b5c52
akbar-sys <61173163+akbar-sys@users.noreply.github.com>
2020-12-22 14:26:53 +0800

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,8 +10,7 @@ class HomeController extends Controller
     public function index()
     {
         $hotels = Hotel::latest()->paginate(6);
-        return view('home.index',compact('hotels'))
-    /**
+        return view('home.index',compact('hotels'));
     }
     public function show($id)
     {
